04f0e6ccd33d925ce688b7933d5e78c4af896250
Luciana <luciana342010@hotmail.com>
2024-06-30 17:33:50 -0300

    finalizando a sessao de nosso servico

---
+++ baralho-cigano.js
@@ -0,0 +1,23 @@
+// const prevButton = document.querySelector('.prev-button');
+// const nextButton = document.querySelector('.next-button');
+// const navLinks = document.querySelectorAll('.nav-link');
+
+// prevButton.addEventListener('click', () => {
+//     let activeLink = document.querySelector('.nav-link.active');
+//     if (activeLink) {
+//         let prevIndex = activeLink.dataset.index - 1;
+//         if (prevIndex >= 0) {
+//             navLinks[prevIndex].click();
+//         }
+//     }
+// });
+
+// nextButton.addEventListener('click', () => {
+//     let activeLink = document.querySelector('.nav-link.active');
+//     if (activeLink) {
+//         let nextIndex = activeLink.dataset.index + 1;
+//         if (nextIndex < navLinks.length) {
+//             navLinks[nextIndex].click();
+//         }
+//     }
+// });
